e7539e1dd6d0d914d41cff92d70375d6bf43bf0d
brendadev <brenda@koneksahealth.com>
2018-08-16 08:17:01 -0400

    added artcile loader

---
+++ app/src/main/java/com/example/android/technewsapp/ArticleLoader.java
@@ -0,0 +1,30 @@
+package com.example.android.technewsapp;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+public class ArticleLoader extends AsyncTaskLoader<List<Article>> {
+    private static final String TAG = ArticleLoader.class.getSimpleName();
+    private String mUrl;
+
+    public ArticleLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<Article> onLoadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+        List<Article> articles = QueryUtils.fetchArticlesData(mUrl);
+        return articles;
+    }
+}
